780b706f52d815b4efea713bafc28c75645f0058
Klaid <klaid1983@gmail.com>
2013-03-23 14:29:40 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Admin
+ * Date: 23.03.13
+ * Time: 14:27
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo 'Test';
